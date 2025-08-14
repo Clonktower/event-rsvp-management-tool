9c7085bc57d5362606c3d96ea7cb380e417325a3
Hemang Bhogayata <hemang.bhogayata@adevinta.com>
2025-08-14 20:00:28 +0200

    chore: cleanup

---
+++ server/src/controllers/rsvpController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { rsvpToEventService } from "../services/rsvp";
+
+export const rsvpToEvent = async (req: Request, res: Response, next: Function) => {
+  try {
+    const { id } = req.params;
+    let { attendeeId, name, status, guests } = req.body;
+    if (!name || !status) {
+      return res.status(400).json({ error: "Name and RSVP status are required." });
+    }
+    if (!attendeeId) {
+      // Generate a new UUID for attendeeId if not provided
+      attendeeId = require('uuid').v4();
+    }
+    const rsvp = await rsvpToEventService({ eventId: id, attendeeId, name, status, guests });
+    res.status(201).json({ message: "RSVP recorded!", rsvp });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const rsvpController = {
+  rsvpToEvent,
+};
