607fbc7e9fc9a0944e3fe915c630170ea5550a60
hemang249 <hemang249@gmail.com>
2025-08-15 14:36:14 +0200

    feat: add login FE

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,6 +6,7 @@ import {authenticateAdmin} from "./middlewares/auth";
 const router = Router();
 
 // Admin routes
+router.post("/admin/login", authenticateAdmin, (_, res) => res.status(200).send())
 router.post("/admin/create-event", authenticateAdmin, eventController.createEvent);
 router.get("/admin/events", authenticateAdmin, eventController.getAllEvents);
 router.delete("/admin/events/:id", authenticateAdmin, eventController.deleteEvent);
