52a220229ebbc4f2837f2f1a2ac7b7ca84e709b1
hemang249 <hemang249@gmail.com>
2025-08-15 22:00:44 +0200

    fix: send auth header for create event request

--- client/src/routes/create-event/+page.svelte
+++ client/src/routes/create-event/+page.svelte
@@ -32,7 +32,7 @@
     }
     loading = true;
     try {
-      const response = await fetch('http://localhost:3000/admin/create-event', {
+      const response = await adminFetch('http://localhost:3000/admin/create-event', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, date, startTime, endTime, maxAttendees, location })
