92c909ad96718120ba840a23f3124db6b3dfdd25
hemang249 <hemang249@gmail.com>
2025-09-13 10:53:02 +0200

    chore: update testIds

--- server/src/controllers/rsvpController.ts
+++ server/src/controllers/rsvpController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { rsvpToEventServiceLegacy, deleteRsvpById, rsvpToEventService, updateRsvpByToken } from "../services/rsvp";
 
-const legacyEventIds: string[]  = ["fe7e8308-386d-499d-bc74-dfaf21e34a9f"]
+const legacyEventIds: string[]  = [
+  "fe7e8308-386d-499d-bc74-dfaf21e34a9f", // local dev
+  "da6722ce-3ff5-49ff-b6ea-8a615f454ee8", // TU
+  "46da7671-9bd3-44c2-a34e-5ec2caaeed88", // PROD test
+]
 
 export const rsvpToEvent = async (req: Request, res: Response, next: Function) => {
   try {
