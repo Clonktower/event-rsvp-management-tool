d08aea399df2a58a6f1325b48d9accb4b73c6a6d
Teppo Lehtonen <69015432+TL6@users.noreply.github.com>
2026-01-24 09:19:27 +0100

    chore: use hostname to connect to server in dev mode (#32)
    This allows using other devices for testing.

--- client/src/utils/apiHost.ts
+++ client/src/utils/apiHost.ts
@@ -1,5 +1,9 @@
+import { browser } from "$app/environment";
+
 const isProd = import.meta.env.MODE === "production";
 
 export const API_HOST = isProd
   ? "https://api.rsvp.clonktower.de"
-  : "http://localhost:3000";
+  : browser
+    ? `http://${window.location.hostname}:3000`
+    : "http://localhost:3000";
